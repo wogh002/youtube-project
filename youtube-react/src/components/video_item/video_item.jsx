df073f3db4ecbae8bb19fc318e67726fd1be0f8a
wogh002 <ekekekem159@gmail.com>
2021-06-20 22:38:27 +0900

    added main page style css

--- youtube-react/src/components/video_item/video_item.jsx
+++ youtube-react/src/components/video_item/video_item.jsx
@@ -3,17 +3,19 @@ import style from './video_item.module.css';
 
 const VideoItems = ({ video, video: { snippet }, onSelect, display }) => {
     //display === list 라면 셀렉티드 된 비디오가 있다는 것.
-    console.log(display);
     const displayType = display === 'list' ? style.list : style.grid;
     return (
         <li className={`${style.video} ${displayType}`} onClick={() => { onSelect(video) }}>
             <img src={snippet.thumbnails.medium.url} className={style.thumbnail} alt="thumbnails" />
             <div className={style.infoContanier}>
                 <img src={video.thumbnailURL} className={style.channelThumbnail} alt="thumbnail"></img>
-                <h1 className={style.title}>{snippet.title}</h1>
-                <p className={style.channel}>{snippet.channelTitle}</p>
+                <div className={style.channelInfo}>
+                    <h1 className={style.title}>{snippet.title}</h1>
+                    <p className={style.channel}>{snippet.channelTitle}</p>
+                    {/* <span className={style.viewCount}>조회수 {video.statistics.viewCount}</span> */}
+                </div>
             </div>
-        </li >
+        </li>
     )
 }
 
--- youtube-react/src/service/youtube.js
+++ youtube-react/src/service/youtube.js
@@ -7,7 +7,7 @@ class Youtube {
             params: {
                 part: 'snippet',
                 chart: 'mostPopular',
-                maxResults: 2,
+                maxResults: 5,
                 regionCode: 'KR'
             }
         })
@@ -18,31 +18,38 @@ class Youtube {
             params: {
                 part: 'snippet',
                 type: 'video',
-                maxResults: 3,
+                maxResults: 6,
                 regionCode: 'KR',
                 q: query
             }
         })
         return response.data.items.map(item => ({ ...item, id: item.id.videoId }));
     }
-
-    //비동기 : 특정 로직의 실행이 끝날때 까지 기다려주는게 아니라 다른 나머지 코드를 먼저 실행하는것.
-    //async 함수는 항상 promise를 반환합니다. 
-    //만약 async 함수의 반환값이 명시적으로 promise가 아니라면 암묵적으로 promise로 감싸집니다.
     async getChannelInfo(videos, promiseArr = []) {
         for (let i = 0; i < videos.length; i++) {
             const response = await this.httpClient.get('/channels', {
                 params: {
-                    part: 'snippet',
+                    part: 'snippet,statistics',
                     id: videos[i].snippet.channelId
                 }
             })
             promiseArr.push(...response.data.items);
         }
-        // 순회 가능한 객체에 주어진 모든 프로미스가 이행한 후,
+        console.log(promiseArr);
         return Promise.resolve(promiseArr.map(item => item.snippet.thumbnails.default.url));
-        //Promise.all([0,1,2])
     }
+    // async getChannelInfo(videos, promiseArr = []) {
+    //     for (let i = 0; i < videos.length; i++) {
+    //         const response = await this.httpClient.get('/channels', {
+    //             params: {
+    //                 part: 'snippet,statistics',
+    //                 id: videos[i].snippet.channelId
+    //             }
+    //         })
+    //         promiseArr.push(...response.data.items);
+    //     }
+    //     return Promise.resolve(promiseArr.map(item => item.snippet.thumbnails.default.url));
+    // }
 }
 
 
